1d75d796c752d3f46c111eb87d74f41d25a9bb68
centurionoftheseed <rmemmer@carolina.rr.com>
2019-12-22 23:32:41 -0600

    modified main page to add ng-view

---
+++ Source/AngularAndMVC/App/App.js
@@ -0,0 +1,165 @@
+﻿var commonModule = angular.module('common', ['ngRoute']);
+var mainModule = angular.module('main', ['common']);
+
+commonModule.factory('viewModelHelper',
+    function ($http, $q, $window, $location) {
+        return MyApp.viewModelHelper(
+            $http, $q, $window, $location);
+    });
+
+commonModule.factory('validator', function () { return valJs.validator(); });
+
+//mainModule.controller("indexViewModel", function (
+//    $scope, $http, $q, $routeParams, $window,
+//    $location, viewModelHelper) {
+
+//    var self = this;
+
+//    $scope.topic =
+//        "Integrating ASP.NET MVC and AngularJS";
+//    $scope.author = "Miguel A. Castro";
+//});
+
+(function (myApp) {
+    var viewModelHelper = function (
+        $http, $q, $window, $location) {
+
+        var self = this;
+
+        self.modelIsValid = true;
+        self.modelErrors = [];
+
+        self.resetModelErrors = function () {
+            self.modelErrors = [];
+            self.modelIsValid = true;
+        }
+
+        self.apiGet = function (uri, data, success, failure, always) {
+            self.modelIsValid = true;
+            $http.get(MyApp.rootPath + uri, data)
+                .then(function (result) {
+                    success(result);
+                    if (always != null)
+                        always();
+                }, function (result) {
+                    if (failure != null) {
+                        failure(result);
+                    }
+                    else {
+                        var errorMessage = result.status + ':' + result.statusText;
+                        if (result.data != null) {
+                            if (result.data.Message != null)
+                                errorMessage += ' - ' + result.data.Message;
+                            if (result.data.ExceptionMessage != null)
+                                errorMessage += ' - ' + result.data.ExceptionMessage;
+                        }
+                        self.modelErrors = [errorMessage];
+                        self.modelIsValid = false;
+                    }
+                    if (always != null)
+                        always();
+                });
+        }
+
+        self.apiPost = function (uri, data, success, failure, always) {
+            self.modelIsValid = true;
+            $http.post(MyApp.rootPath + uri, data)
+                .then(function (result) {
+                    success(result);
+                    if (always != null)
+                        always();
+                }, function (result) {
+                    if (failure != null) {
+                        failure(result);
+                    }
+                    else {
+                        var errorMessage = result.status + ':' + result.statusText;
+                        if (result.data != null) {
+                            if (result.data.Message != null)
+                                errorMessage += ' - ' + result.data.Message;
+                            if (result.data.ExceptionMessage != null)
+                                errorMessage += ' - ' + result.data.ExceptionMessage;
+                        }
+                    }
+                    if (always != null)
+                        always();
+                });
+        }
+
+        self.goBack = function () {
+            $window.history.back();
+        }
+
+        self.navigateTo = function (path, params) {
+            if (params == null)
+                $location.path(MyApp.rootPath + path);
+            else
+                $location.path(MyApp.rootPath + path).search(params);
+        }
+
+        self.refreshPage = function (path) {
+            $window.location.href = MyApp.rootPath + path;
+        }
+
+        self.clone = function (obj) {
+            return JSON.parse(JSON.stringify(obj))
+        }
+
+        self.querystring = function (param) {
+            if ($location.search != null)
+                return $location.search()[param];
+            else
+                return null;
+        }
+
+        self.resetQueryParams = function () {
+            $location.url($location.path());
+        }
+
+        return this;
+    };
+    myApp.viewModelHelper = viewModelHelper;
+}(window.MyApp));
+
+
+
+
+
+
+
+
+
+mainModule.config(function ($routeProvider,
+    $locationProvider) {
+    $routeProvider.when('/', {
+        templateUrl: '/App/Customer/Views/MainHomeView.html',
+        controller: 'mainHomeViewModel'
+    });
+    //$routeProvider.when('/customer/list', {
+    //    templateUrl: '/App/Customer/Views/CustomerListView.html',
+    //    controller: 'customerListViewModel'
+    //});
+    //$routeProvider.when('/customer/show/:customerId', {
+    //    templateUrl: '/App/Customer/Views/CustomerView.html',
+    //    controller: 'customerViewModel'
+    //});
+    //$routeProvider.when('/', {
+    //    templateUrl: '/App/Customer/Views/MainHomeView.html',
+    //    controller: 'mainHomeViewModel'
+    //});
+
+
+    //$routeProvider.otherwise({
+    //    redirectTo: '/customer'
+    //});
+
+
+
+
+
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+});
---
+++ Source/AngularAndMVC/App/Customer/ViewModels/mainHomeViewModel.js
@@ -0,0 +1,75 @@
+﻿mainModule.controller("mainHomeViewModel", function (
+    $scope, $http, $q, $routeParams, $window,
+    $location, viewModelHelper) {
+
+    var self = this;
+
+    $scope.topic =
+        "Integrating ASP.NET MVC and AngularJS";
+    $scope.author = "Miguel A. Castro";
+
+
+    $scope.viewModelHelper = viewModelHelper;
+    //$scope.customerService = customerService;
+    $scope.pageHeading = "test from main module";
+
+    var initialize = function () {
+        $scope.refreshCustomers();
+    }
+
+    $scope.refreshCustomers = function () {
+        console.log("inside refresh customers");
+        viewModelHelper.apiGet('api/customers', null,
+            function (result) {
+                $scope.customers = result.data;
+                console.table($scope.customers);
+            });
+    }
+
+    //$scope.showCustomer = function (customer) {
+    //    $scope.flags.shownFromList = true; // note this object is declared in the RootViewModel
+    //    viewModelHelper.navigateTo('customer/show/' + customer.CustomerId);
+    //}
+
+    //initialize();
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//mainModule.controller("mainHomeViewModel", function ($scope, customerService, $http, $q, $routeParams, $window, $location, viewModelHelper) {
+
+//    $scope.viewModelHelper = viewModelHelper;
+//    $scope.customerService = customerService;
+//    $scope.TEST_FROM_MAIN_MODULE = "test from main module";
+
+//    var initialize = function () {
+//        $scope.refreshCustomers();
+//    }
+
+//    $scope.refreshCustomers = function () {
+//        viewModelHelper.apiGet('api/customers', null,
+//            function (result) {
+//                $scope.customers = result.data;
+//            });
+//    }
+
+//    $scope.showCustomer = function (customer) {
+//        $scope.flags.shownFromList = true; // note this object is declared in the RootViewModel
+//        viewModelHelper.navigateTo('customer/show/' + customer.CustomerId);
+//    }
+
+//    initialize();
+//});
